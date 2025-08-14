ef14d10e6bdd42b0b48d1757b887746cab12867a
ghost <keshav.bgir.980@gmail.com>
2025-08-14 09:59:24 +0530

    remove some spaces

--- arrays/05-rowWithMax1s.js
+++ arrays/05-rowWithMax1s.js
@@ -1,8 +1,6 @@
 /*
   You are given a 2D binary array arr[][] consisting of only 1s and 0s. Each row of the array is sorted in non-decreasing order. Your task is to find and return the index of the first row that contains the maximum number of 1s. If no such row exists, return -1.
-
   Note:
-
   The array follows 0-based indexing.
   The number of rows and columns in the array are denoted by n and m respectively.
 
@@ -11,7 +9,6 @@
   Output: 2
   Explanation: Row 2 contains the most number of 1s (4 1s). Hence, the output is 2.
   */
-
 export const rowWithMax1s = (arr) => {
   const map = new Map();
   for(let i = 0; i < arr.length; i++) {
